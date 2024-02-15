55f5989347c7f2efa4977423fc9fe893f6454731
Amran Haroon <405111+haroon3rd@users.noreply.github.com>
2024-02-15 15:52:30 -0600

    New branch for no-leader

---
+++ EdgeKeeperLinux/src/edu/tamu/cse/lenss/edgekeeper/jmmDNSPlus/JmmDNSPlusHandler.java
@@ -0,0 +1,157 @@
+package edu.tamu.cse.lenss.edgekeeper.jmmDNSPlus;
+
+import org.apache.log4j.Logger;
+
+import edu.tamu.cse.lenss.edgeKeeper.utils.Terminable;
+import javax.jmdns.*;
+
+public class JmmDNSPlusHandler extends Thread implements Terminable  {
+	public static final Logger logger = Logger.getLogger(JmmDNSPlusHandler.class);
+
+	//service variables
+    private String serviceType = "_http._tcp.local.";
+    private String serviceName = "desktop_instance";
+    private String serviceText = "GUID000AAA";
+    int servicePort = 0;
+    JmmDNS registry;
+    ServiceInfo serviceInfo;
+    SampleListener sampleListener;
+
+//    public JmmDNSPlusHandler(EKHandler ekHandler) {
+//		// TODO Auto-generated constructor stub
+//	}
+
+
+	//constructor
+    public JmmDNSPlusHandler(String serviceType, String serviceName, String serviceText ){
+    	this.serviceName = serviceName;
+    	this.serviceText = serviceText;
+    	this.serviceType = serviceType;
+    }
+    
+    public JmmDNSPlusHandler(){
+    }
+
+
+    @Override
+    public void run(){
+        try{
+        	logger.debug("Starting jmmDNS Service");
+            //get new jmmdns instance
+            registry = JmmDNS.Factory.getInstance();
+
+            //sampleListener
+            sampleListener = new SampleListener();
+
+            //serviceinfo
+            
+            logger.debug("AMRAN_JMMDNS" + serviceType +" "+ serviceName +" "+ servicePort);
+            
+            serviceInfo = ServiceInfo.create(serviceType, serviceName, servicePort, 1, 1, false,  encodeServiceText(serviceText));
+
+            //register service
+            registry.registerService(serviceInfo);
+
+            //add service listener
+            registry.addServiceListener(serviceType, sampleListener);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    //ServiceListener callback functions
+    private static class SampleListener implements ServiceListener {
+
+        public SampleListener(){}
+
+        @Override
+        public void serviceAdded(ServiceEvent event) {
+            final String name = event.getInfo().getName();
+            final String text = decodeServiceText(new String(event.getInfo().getTextBytes()));
+            final String[] ips = event.getInfo().getHostAddresses();
+            String ip="";
+            for(String aipee:ips){ip+=aipee + " ";}
+
+            logger.debug("_NSD_ Service Added:" + " Name: " + name + ", Text: " + text + " ips: " + ip);
+        }
+
+        @Override
+        public void serviceRemoved(ServiceEvent event) {
+            final String name = event.getInfo().getName();
+            final String text = decodeServiceText(new String(event.getInfo().getTextBytes()));
+            final String[] ips = event.getInfo().getHostAddresses();
+            String ip="";
+            for(String aipee:ips){ip+=aipee + " ";}
+
+            logger.debug("_NSD_ Service Removed:" + " Name: " + name + ", Text: " + text + " ips: " + ip);
+
+        }
+
+        @Override
+        public void serviceResolved(ServiceEvent event) {
+            final String name = event.getInfo().getName();
+            final String text = decodeServiceText(new String(event.getInfo().getTextBytes()));
+            final String[] ips = event.getInfo().getHostAddresses();
+            String ip="";
+            for(String aipee:ips){ip+=aipee + " ";}
+
+            logger.debug("_NSD_ Service Resolved:" + " Name: " + name + ", Text: " + text + " ips: " + ip);
+
+        }
+
+    }
+
+    //only used for android
+    public void enableNeighborDiscoveryOnLTE(){
+        if (registry!=null){
+            //enable lte support
+            ServiceInfo serviceInfoLTE = ServiceInfo.create("LTE_LTE_LTE", serviceName, servicePort, 1, 1, true,  encodeServiceText("LTE_LTE_LTE"));
+            registry.enableLTEsupport(serviceInfoLTE, sampleListener);
+        }
+    }
+
+    //only used for android
+    public void disableNeighborDiscoveryOnLTE(){
+        if (registry!=null){
+            registry.disableLTEsupport();
+        }
+    }
+
+    //adds encoding with a service text before use.
+    public static String encodeServiceText(String serviceText){
+        //Add '!@# before and '$%^' after serviceText
+        return "!@#" + serviceText + "$%^";
+    }
+
+    //decode a service text before
+    public static String decodeServiceText(String serviceText){
+        //check if the string has '!@# before and '$%^' after
+        if(serviceText.contains("!@#") && serviceText.contains("$%^")){
+            return serviceText.substring(serviceText.indexOf("!@#") + "!@#".length(), serviceText.indexOf("$%^"));
+        }
+
+        return "<empty>";
+    }
+
+    //change service text on the fly
+    public void setText(String serviceText){
+        serviceInfo.setText(encodeServiceText(serviceText).getBytes());
+    }
+    
+    @Override
+    public void terminate() {
+        try {
+            if (this.registry != null) {
+                this.registry.unregisterAllServices();
+                this.registry.close();
+                //this.registry.disableLTEsupport();
+                System.out.println("terminated jmdns stuff successfully");
+            }
+        } catch (Exception ex) {
+            System.out.println("error in terminating jmdns stuff");
+            ex.printStackTrace();
+        }
+    }
+
+}
